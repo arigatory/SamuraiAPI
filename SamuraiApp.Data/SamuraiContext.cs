ab1883bd4b2138b29e0070c1b80f3772bfcb7716
Ivan MSI <arigatory@gmail.com>
2022-03-26 23:10:44 +0300

    run

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,27 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options)
+            : base(options)
+        {
+
+        }
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var connectionString = "Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData";
-            optionsBuilder.UseSqlServer(connectionString)
-                .LogTo(Console.WriteLine, new[] {DbLoggerCategory.Database.Command.Name}, LogLevel.Information)
-                .EnableSensitiveDataLogging();
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -30,8 +22,8 @@ namespace SamuraiApp.Data
                 .WithMany(b => b.Samurais)
                 .UsingEntity<BattleSamurai>
                 (bs => bs.HasOne<Battle>().WithMany(),
-                bs=> bs.HasOne<Samurai>().WithMany())
-                .Property(bs=>bs.DateJoined)
+                bs => bs.HasOne<Samurai>().WithMany())
+                .Property(bs => bs.DateJoined)
                 .HasDefaultValueSql("getdate()");
         }
     }
